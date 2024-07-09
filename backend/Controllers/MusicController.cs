d3766a470cd6f8cf7228f4ed1f0215ab66150e29
pendevx <pendevx@gmail.com>
2024-07-09 22:13:55 +1200

    base stuffs

---
+++ backend/Controllers/MusicController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("music")]
+public class MusicController : ControllerBase
+{
+
+}
---
+++ backend/Program.cs
@@ -0,0 +1,28 @@
+namespace backend;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}

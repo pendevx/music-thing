8c9d67c68268272bef7874fe5f41f2374e521838
pendevx <pendevx@gmail.com>
2024-07-15 22:31:39 +1200

    dockerfile

---
+++ backend/docker.ps1
@@ -0,0 +1,5 @@
+﻿$imageName = "music-backend-image"
+$containerName = "music-backend"
+
+docker build -t $imageName -f Dockerfile .
+docker run -d -p 8080:8080 -v $PSScriptRoot/assets:/backend/assets --name $containerName $imageName

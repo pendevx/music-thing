9e2d0850e5b11ac55451f49d08decc791600cd7d
pendevx <pendevx@gmail.com>
2024-07-18 01:35:11 +1200

    urls refactor

--- webapp/src/utils/url-builder.api.js
+++ webapp/src/utils/url-builder.api.js
@@ -1,11 +1,13 @@
+const baseUrl = `//${location.hostname}:${import.meta.env.VITE_API_PORT}/`;
+
 export function buildSongUrl(songKey) {
-    return "//" + location.hostname + ":" + import.meta.env.VITE_API_PORT + "/music/download/" + encodeURIComponent(songKey);
+    return `${baseUrl}music/download/${encodeURIComponent(songKey)}`;
 }
 
 export function buildListUrl() {
-    return "//" + location.hostname + ":" + import.meta.env.VITE_API_PORT + "/music/list";
+    return `${baseUrl}music/list`;
 }
 
 export function buildLyricsUrl(songKey) {
-    return "//" + location.hostname + ":" + import.meta.env.VITE_API_PORT + "/lyrics/download/" + encodeURIComponent(songKey);
+    return `${baseUrl}lyrics/download/${encodeURIComponent(songKey)}`;
 }

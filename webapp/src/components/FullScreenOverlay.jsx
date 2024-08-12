d2aab95da339bd74e01b092260f51727d0f646cd
pendevx <pendevx@gmail.com>
2024-08-13 10:38:10 +1200

    small progress

--- webapp/src/components/FullScreenOverlay.jsx
+++ webapp/src/components/FullScreenOverlay.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MusicContext } from "../contexts/MusicContext";
-import { MusicProgressBar, Scrollable } from "./";
+import { MusicProgressBar } from "./";
 import messageBus from "../utils/MessageBus";
 import { formatTime } from "../utils/formats";
 import { MusicIconSvg, PlayBehaviourIcon, MusicPausedSvg, MusicPlaySvg, ChangeTrack } from "../icons";
@@ -8,7 +8,6 @@ import { MusicIconSvg, PlayBehaviourIcon, MusicPausedSvg, MusicPlaySvg, ChangeTr
 export default function FullScreenOverlay({ hideFullscreen }) {
     const [totalDuration, setTotalDuration] = React.useState(0);
     const [currentTime, setCurrentTime] = React.useState(0);
-    const imgRef = React.useRef(null);
     const musicContext = React.useContext(MusicContext);
 
     React.useEffect(function () {
@@ -62,46 +61,40 @@ export default function FullScreenOverlay({ hideFullscreen }) {
     }
 
     return (
-        <Scrollable showScroller={false} className="absolute inset-0 overflow-auto py-16 text-white">
-            <div className="midmobile:w-[70%] lgmobile:w-[60%] lglaptop:w-[35%] xllaptop:w-[30%] mx-auto flex w-4/5 flex-col items-center gap-4 tablet:w-[50%] laptop:w-[40%] desktop:w-1/4">
-                <h2 className="text-center text-3xl">{musicContext.songName()}</h2>
+        <div className="absolute left-0 right-0 flex h-full justify-center py-16 text-white">
+            <div className="absolute bottom-0 top-0 grid w-3/5 grid-rows-[2fr,1fr,5fr,1fr,1fr,1fr] gap-4 tablet:w-1/2 laptop:w-1/4">
+                <h2 className="text mx-auto text-center text-3xl">{musicContext.songName()}</h2>
 
-                <h3 className="mt-[-10px] text-center text-xl">{"Artist" ?? musicContext.artist()}</h3>
+                <h3 className="mx-auto text-center text-xl">{"Artist" ?? musicContext.artist()}</h3>
 
-                <div className="aspect-square w-full rounded-2xl bg-white p-10 tablet:p-16 laptop:p-20 desktop:p-24">
-                    {/* <img src="asdf" alt="" ref={imgRef} />
-
-                        {!imgRef.current?.complete && <MusicIconSvg />} */}
-
+                <div className="mx-auto aspect-square w-full rounded-2xl bg-white p-24">
                     <MusicIconSvg />
                 </div>
 
                 <MusicProgressBar songDurationSecs={totalDuration} currentTime={currentTime} onFastForward={onFastForward} />
 
-                <span className="textfont-semibold mt-[-10px]">
+                <span className="textfont-semibold w-full text-center">
                     {formatTime(currentTime)} / {formatTime(totalDuration)}
                 </span>
 
-                <div className="w-full px-2">
-                    <div className="midmobile:gap-x-20 grid grid-cols-3 grid-rows-2 gap-x-16 gap-y-5 desktop:gap-x-28">
-                        <div className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800">?</div>
-                        <div className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800">?</div>
-                        <div className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800" onClick={nextPlayBehaviour}>
-                            <PlayBehaviourIcon playBehaviour={musicContext.playBehaviour} className="lgmobile:p-1 h-full w-full fill-white tablet:p-2" />
-                        </div>
-                        <div
-                            className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800 tablet:p-2"
-                            onClick={() => musicContext.previous()}>
-                            <ChangeTrack className="rotate-180" />
-                        </div>
-                        <div className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800 tablet:p-2" onClick={handlePlayPause}>
-                            {musicContext.isPlaying ? <MusicPausedSvg /> : <MusicPlaySvg />}
-                        </div>
-                        <div
-                            className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800 tablet:p-2"
-                            onClick={() => musicContext.next()}>
-                            <ChangeTrack />
-                        </div>
+                <div className="grid grid-cols-3 grid-rows-2 px-2">
+                    <div className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800">?</div>
+                    <div className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800">?</div>
+                    <div className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800" onClick={nextPlayBehaviour}>
+                        <PlayBehaviourIcon playBehaviour={musicContext.playBehaviour} className="lgmobile:p-1 h-full w-full fill-white tablet:p-2" />
+                    </div>
+                    <div
+                        className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800 tablet:p-2"
+                        onClick={() => musicContext.previous()}>
+                        <ChangeTrack className="rotate-180" />
+                    </div>
+                    <div className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800 tablet:p-2" onClick={handlePlayPause}>
+                        {musicContext.isPlaying ? <MusicPausedSvg /> : <MusicPlaySvg />}
+                    </div>
+                    <div
+                        className="flex h-full cursor-pointer items-center justify-center rounded-[50%] transition-colors duration-300 hover:bg-gray-800 tablet:p-2"
+                        onClick={() => musicContext.next()}>
+                        <ChangeTrack />
                     </div>
                 </div>
             </div>
@@ -109,6 +102,6 @@ export default function FullScreenOverlay({ hideFullscreen }) {
             <button className="fixed bottom-1 left-1 w-fit cursor-pointer px-8 py-4 text-white transition-colors duration-100 hover:bg-zinc-800" onClick={hideFullscreen}>
                 Back
             </button>
-        </Scrollable>
+        </div>
     );
 }

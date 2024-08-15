d353966aee4b3c8d9bc456b9d3206a4a2bad1cbb
pendevx <pendevx@gmail.com>
2024-08-16 00:40:02 +1200

    play behaviours better

--- webapp/src/utils/playBehaviour.js
+++ webapp/src/utils/playBehaviour.js
@@ -1,42 +1,11 @@
+export const playBehaviours = ["loop", "shuffle", null];
+
 export function nextPlayBehaviour(currentPlayBehaviour) {
-    let nextBehaviour = "";
-
-    switch (currentPlayBehaviour) {
-        case "loop":
-            nextBehaviour = "shuffle";
-            break;
-
-        case "shuffle":
-            nextBehaviour = null;
-            break;
-
-        default:
-            nextBehaviour = "loop";
-            break;
-    }
-
+    let nextBehaviour = playBehaviours[(playBehaviours.indexOf(currentPlayBehaviour) + 1) % playBehaviours.length];
     return nextBehaviour;
 }
 
 export function lastPlayBehaviour(currentPlayBehaviour) {
-    // let lastBehaviour = "";
-
-    // switch (currentPlayBehaviour) {
-    //     case "loop":
-    //         lastBehaviour = null;
-    //         break;
-
-    //     case "shuffle":
-    //         lastBehaviour = "loop";
-    //         break;
-
-    //     default:
-    //         lastBehaviour = "shuffle";
-    //         break;
-    // }
-
-    // return lastBehaviour;
-
-    // cuz y not lol
-    return nextPlayBehaviour(nextPlayBehaviour(currentPlayBehaviour));
+    let lastBehaviour = playBehaviours[(playBehaviours.indexOf(currentPlayBehaviour) - 1) % playBehaviours.length];
+    return lastBehaviour;
 }

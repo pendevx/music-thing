029c0512b918d43de20cc5d6693fad7c4ad0edd8
pendevx <pendevx@gmail.com>
2024-09-08 14:08:39 +1200

    Forgot the program.cs lol

---
+++ backend/Music.Database/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Music.Database;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var serviceCollection = new ServiceCollection();
+
+        var thisAssembly = Assembly.GetExecutingAssembly();
+        var embeddedResources = thisAssembly.GetManifestResourceNames();
+
+        foreach (var resource in embeddedResources)
+        {
+            Console.WriteLine(resource);
+
+            var content = thisAssembly.GetManifestResourceStream(resource);
+            if (content is null) return;
+
+            var stdout = Console.OpenStandardOutput();
+            content.CopyTo(stdout);
+        }
+    }
+}
+
+/*
+ * database:
+ *  store what scripts have been run
+ *  store what time the last update occurred
+ *
+ * updater:
+ *  grab last update time
+ *  binary search through file system to find the first script which hasn't run BASED ON 'created on' // WONT WORK: if re-clone repository, 'created on' is changed!
+ *  validate those scripts haven't been run yet
+ *  run the scripts
+ */

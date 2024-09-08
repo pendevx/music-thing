898a2561c3323a5aca436b8a089abbb4f192caed
pendevx <pendevx@gmail.com>
2024-09-08 14:09:04 +1200

    initial scripts

--- backend/Music.Database/Scripts/Database_Init.sql
+++ backend/Music.Database/Scripts/Database_Init.sql
@@ -1,1 +1,13 @@
-﻿
+﻿-- Create database
+IF NOT EXISTS(
+    SELECT *
+    FROM sys.databases
+    WHERE name = 'MusicThing'
+)
+    BEGIN
+        CREATE DATABASE [MusicThing]
+    END
+GO
+
+USE [MusicThing]
+GO

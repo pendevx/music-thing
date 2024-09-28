0a2dca3ba447b7d856ac19e20e209b7b765757c1
pendevx <pendevx@gmail.com>
2024-09-29 05:34:57 +1300

    add token to versions

--- backend/Music.Database/Versions/Sessions.sql
+++ backend/Music.Database/Versions/Sessions.sql
@@ -4,9 +4,10 @@ IF OBJECT_ID('Sessions', 'U') IS NULL
         CREATE TABLE Sessions
         (
             Id        INT PRIMARY KEY IDENTITY (1,1),
-            AccountId INT       NOT NULL REFERENCES Accounts (Id),
-            CreatedOn DATETIME2 NOT NULL,
-            ExpiresOn DATETIME2 NOT NULL
+            AccountId INT              NOT NULL REFERENCES Accounts (Id),
+            CreatedOn DATETIME2        NOT NULL,
+            ExpiresOn DATETIME2        NOT NULL,
+            Token     UNIQUEIDENTIFIER NOT NULL UNIQUE DEFAULT '00000000-0000-0000-0000-000000000000'
         )
 
     END

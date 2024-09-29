87f21bcb53c209a4d6e48c9b744288d07e7724eb
pendevx <pendevx@gmail.com>
2024-09-29 15:42:11 +1300

    update accounts version file

--- backend/Music.Database/Versions/Accounts.sql
+++ backend/Music.Database/Versions/Accounts.sql
@@ -5,9 +5,10 @@ IF OBJECT_ID('Accounts', 'U') IS NULL
         (
             Id             INT PRIMARY KEY IDENTITY (1,1),
             Guid           UNIQUEIDENTIFIER NOT NULL UNIQUE,
-            Username       NVARCHAR(256)    NOT NULL,
+            Username       NVARCHAR(256)    NOT NULL UNIQUE,
             SaltedPassword VARBINARY(MAX)   NOT NULL,
-            CreatedOn      DATETIME2        NOT NULL
+            CreatedOn      DATETIME2        NOT NULL,
+            DisplayName    NVARCHAR(30)     NOT NULL DEFAULT ''
         )
 
     END

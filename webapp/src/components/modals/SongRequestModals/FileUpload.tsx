313c49e349e2040d7974760a3e3ed81a6a161fb6
pendevx <pendevx@gmail.com>
2024-11-14 08:34:25 +1300

    center properly

--- webapp/src/components/modals/SongRequestModals/FileUpload.tsx
+++ webapp/src/components/modals/SongRequestModals/FileUpload.tsx
@@ -6,7 +6,7 @@ export default function FileUpload({ reportValidity }: UploaderProps) {
         <div className="flex flex-col gap-4">
             <RaisedInputPlaceholder
                 className="block w-full rounded-br-lg rounded-tl-lg border-[1px] border-solid border-white p-2"
-                colorClass="text-white bg-[#080808] px-2"
+                colorClass="text-white bg-[#080808] px-2 h-fit bottom-0 top-0 "
                 index={0}
                 placeholder="Title"
                 required
@@ -19,7 +19,7 @@ export default function FileUpload({ reportValidity }: UploaderProps) {
                 type="file"
                 name="file"
                 required
-                className="block w-full rounded-br-lg rounded-tl-lg border-[1px] border-solid border-white p-2 text-white"
+                className="bottom-0 top-0 block h-fit w-full rounded-br-lg rounded-tl-lg border-[1px] border-solid border-white p-2 text-white"
                 onChange={e => reportValidity(1, !!e.target.value)}
             />
         </div>
--- webapp/src/components/modals/SongRequestModals/UrlUpload.tsx
+++ webapp/src/components/modals/SongRequestModals/UrlUpload.tsx
@@ -2,11 +2,13 @@ import { RaisedInputPlaceholder } from "../..";
 import { UploaderProps } from "./types";
 
 export default function UrlUpload({ reportValidity }: UploaderProps) {
+    const options = ["YouTube", "YouTube Music", "Soundcloud"];
+
     return (
         <div className="flex flex-col gap-4">
             <RaisedInputPlaceholder
                 className="block w-full rounded-br-lg rounded-tl-lg border-[1px] border-solid border-white p-2"
-                colorClass="text-white bg-[#080808] px-2"
+                colorClass="text-white bg-[#080808] px-2 h-fit bottom-0 top-0 "
                 index={0}
                 placeholder="Title"
                 required
@@ -18,7 +20,7 @@ export default function UrlUpload({ reportValidity }: UploaderProps) {
             <div className="grid grid-cols-2 gap-4">
                 <RaisedInputPlaceholder
                     className="block w-full rounded-br-lg rounded-tl-lg border-[1px] border-solid border-white p-2"
-                    colorClass="text-white bg-[#080808] px-2"
+                    colorClass="text-white bg-[#080808] px-2 h-fit bottom-0 top-0 "
                     index={1}
                     placeholder="URL"
                     required
@@ -31,15 +33,11 @@ export default function UrlUpload({ reportValidity }: UploaderProps) {
                     <option className="text-black" value="">
                         Please select
                     </option>
-                    <option className="text-black" value="YouTube">
-                        YouTube
-                    </option>
-                    <option className="text-black" value="YouTube Music">
-                        YouTube Music
-                    </option>
-                    <option className="text-black" value="Soundcloud">
-                        Soundcloud
-                    </option>
+                    {options.map((option, i) => (
+                        <option key={option} className="text-black" value={option}>
+                            {option}
+                        </option>
+                    ))}
                 </select>
             </div>
         </div>

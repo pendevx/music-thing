472691dee63043d0050ffc8f384193bf2a63c882
pendevx <pendevx@gmail.com>
2024-12-21 15:36:00 +1300

    use dbcontext

--- backend/Music.CommandHandlers/Audio/SilentlyUploadSongHandler.cs
+++ backend/Music.CommandHandlers/Audio/SilentlyUploadSongHandler.cs
@@ -1,15 +1,16 @@
 using Music.Repositories.Contracts;
+using Music.Repository.EF.DatabaseContexts;
 using Music.Repository.EF.Models.Generated;
 
 namespace Music.CommandHandlers.Audio;
 
 public class SilentlyUploadSongHandler : IBaseCommandHandler
 {
-    private readonly ISongRepository _songRepository;
+    private readonly MusicContext _musicContext;
 
-    public SilentlyUploadSongHandler(ISongRepository songRepository)
+    public SilentlyUploadSongHandler(MusicContext musicContext)
     {
-        _songRepository = songRepository;
+        _musicContext = musicContext;
     }
 
     public void Execute()
@@ -19,13 +20,15 @@ public class SilentlyUploadSongHandler : IBaseCommandHandler
         foreach (var file in files)
         {
             var content = File.ReadAllBytes(file);
-            _songRepository.Create(new Song
+            _musicContext.Songs.Add(new Song
             {
                 Contents = content,
                 Name = file[(file.LastIndexOf('\\') + 1)..],
                 MimeType = "audio/mpeg"
             });
         }
+
+        _musicContext.SaveChanges();
 
         // Remove once added
         foreach (var file in files)

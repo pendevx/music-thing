2c68ff6f879f857edfa4631668425bdd1c6f8890
pendevx <pendevx@gmail.com>
2024-12-23 10:36:05 +1300

    DS-9: service class for checking

--- backend/Music.Services/Validators/UserActionAccessValidator.cs
+++ backend/Music.Services/Validators/UserActionAccessValidator.cs
@@ -1,15 +1,31 @@
+using System.Security.Authentication;
+using Music.Global.Contracts;
 using Music.Repository.EF.DatabaseContexts;
+using Music.Repository.EF.Models.Generated;
 
 namespace Music.Services.Validators;
 
-public class ValidatePermissionService
+public class UserActionAccessValidator
 {
     private readonly MusicContext _dbContext;
+    private readonly IAuthContext _authContext;
 
-    public ValidatePermissionService(MusicContext dbContext)
+    public UserActionAccessValidator(MusicContext dbContext, IAuthContext authContext)
     {
         _dbContext = dbContext;
+        _authContext = authContext;
     }
 
-    public void
+    public void CheckCurrentAccountHasRole(Role role)
+    {
+        var account = _authContext.GetAccount();
+
+        if (account is null)
+            throw new AuthenticationException("not logged in");
+
+        var isPermitted = _dbContext.AccountRoles.Any(ar => ar.AccountId == account.Id && ar.RoleId == role.Id);
+
+        if (!isPermitted)
+            throw new AuthenticationException("not allowed");
+    }
 }
